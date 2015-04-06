1e3731a78dbe4583097d423ec26e3ca67c1b8837
njelin00 <njelin00@fesb.hr>
2015-04-06 14:45:32 +0200

    First commit

---
+++ DateProject/Date.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateProject
+{
+    public class Date
+    {
+        int day, month, year;
+        string[] month_name = new string[12] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+        int[] number_of_days_in_month = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        public Date(int x_day, int x_month, int x_year)
+        {
+            CheckDateFormat(x_day, x_month, x_year);
+            this.day = x_day;
+            this.month = x_month;
+            this.year = x_year;
+        }
+
+        public Date()
+        {
+
+        }
+
+        private void CheckDateFormat(int day, int month, int year)
+        {
+            if (day <= 0 || month <= 0 || year <= 0)
+            {
+                throw new InvalidProgramException("Pogrešan unos. vrijednosti moraju biti pozitivne!");
+            }
+            if (month > 12)
+            {
+                throw new InvalidProgramException("Pogrešan unos! Broj mjeseci ne smije biti veći od 12!");
+            }
+            if (day > number_of_days_in_month[month - 1] || (isLeapYear(year) && month == 2 && day > number_of_days_in_month[month - 1] + 1))
+            {
+                throw new InvalidProgramException("Pogrešan unos! Unesen je broj dana veći nego što ga odabrani mjesec ima!");
+            }
+        }
+
+
+
+        public string getMonthName(int month)
+        {
+            if (month < 0 ||month > 12)
+            {
+                throw new InvalidProgramException("Pogrešan Unos! Uneseni broj mjeseca ne postoji!");
+            }
+            return month_name[month - 1];
+        }
+
+        public string getMonthName()
+        {
+            return month_name[this.month - 1];
+        }
+
+
+
+        public int getNumberOfRemaingDaysInMonth(int day, int month, int year)
+        {
+            if (month == 2 && isLeapYear(year) && month <= 12)
+            {
+                return GetNumberOfDays(day, month) + 1;
+            }
+            return GetNumberOfDays(day, month);
+
+        }
+
+        public int getNumberOfRemaingDaysInMonth(int day, int month)
+        {
+            if (month == 2)
+            {
+                throw new InvalidProgramException("Veljača ovisno o godini ima drugačiji broj dana!");
+            }
+            return GetNumberOfDays(day, month);
+        }
+
+
+
+
+        private int GetNumberOfDays(int day, int month)
+        {
+            if (day > number_of_days_in_month[month - 1])
+            {
+                throw new InvalidProgramException("Unijeli ste veći broj dana nego što ih odabrani mjesec ima!");
+            }
+            return number_of_days_in_month[month - 1] - day;
+        }
+
+        public int getNumberOfRemaingDaysInMonth()
+        {
+            if (this.month == 2 && this.isLeapYear() && this.month <= 12)
+            {
+                return number_of_days_in_month[this.month - 1] - this.day + 1;
+            }
+            return number_of_days_in_month[this.month - 1] - this.day;
+        }
+
+
+        //racunamo je li godina prijestupna. ona koja je prijestupna je djeljiva sa 4 ako nije djeljiva sa 100
+        //prijestupnim godinama dodajemo i one koje su djeljive sa 400
+        public bool isLeapYear(int year)
+        {
+            return ((year%4 == 0 && year%10 != 0) || year%400 == 0);
+        }
+
+        public bool isLeapYear()
+        {
+            return ((this.year%4 == 0 && this.year%10 != 0) || this.year%400 == 0);
+        }
+
+    }
+}
---
+++ DateProjectTest/Date_test.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DateProject;
+
+namespace DateProjectTest
+{
+    [TestClass]
+    public class Date_test
+    {
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestNegativeNumberInDate()
+        {
+            var wallet = new Date(-3, 23, 2011);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestZeroInMonth()
+        {
+            var wallet = new Date(31, 0, 2014);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestUnknownMonth()
+        {
+            var wallet = new Date(11, 13, 2007);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestNumberOfDaysInMonth_TooBig()
+        {
+            var wallet = new Date(31, 4, 2014);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestNumberOfDaysInFebruary_TooBig()
+        {
+            var wallet = new Date(29, 2, 2014);
+        }
+
+
+        [TestMethod]
+        public void TestGetMonth()
+        {
+            var date = new Date();
+            Assert.AreEqual("April", date.getMonthName(4), "Pogreska u izracunu mjeseca!");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestGetMonthError()
+        {
+            var date = new Date();
+            string name = date.getMonthName(13);
+        }
+
+        [TestMethod]
+        public void TestGetMonth1()
+        {
+            var datum = new Date(7, 1, 2013);
+            Assert.AreEqual("January",datum.getMonthName(),"Pogreška u dohvatu mjeseca!");
+        }
+
+        [TestMethod]
+        public void TestGetNumberOfRemainingDays()
+        {
+            var date = new Date();
+            Assert.AreEqual(29,date.getNumberOfRemaingDaysInMonth(2, 5),"Pogreska u izracunu preostalih dana!");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestGetNumberOfDaysInFebruary_Error()
+        {
+            var date = new Date();
+            int number = date.getNumberOfRemaingDaysInMonth(2, 2);
+        }
+
+        [TestMethod]
+        public void TestGetNumberOfRemainingDays_WithDate()
+        {
+            var date = new Date(2, 2, 2015);
+            Assert.AreEqual(26, date.getNumberOfRemaingDaysInMonth(), "Pogreska u izracunu preostalih dana!");
+        }
+
+        [TestMethod]
+        public void TestGetNumberOfRemainingDays_WithDate_LeapYear()
+        {
+            var date = new Date(2, 2, 2008);
+            Assert.AreEqual(27, date.getNumberOfRemaingDaysInMonth(), "Pogreska u izracunu preostalih dana!");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidProgramException))]
+        public void TestGetNumberOfDays_ErrorTooMuchDays()
+        {
+            var date = new Date();
+            int number = date.getNumberOfRemaingDaysInMonth(29, 2, 2014);
+        }
+
+
+        [TestMethod]
+        public void TestIsLeapYear_NotLeapYear()
+        {
+            var date = new Date();
+            Assert.AreEqual(false,date.isLeapYear(2015), "Pogreska u izracunu prijestupne godine");
+        }
+
+        [TestMethod]
+        public void TestIsLeapYear_LeapYear()
+        {
+            var date = new Date();
+            Assert.AreEqual(true, date.isLeapYear(2012), "Pogreska u izracunu prijestupne godine");
+        }
+
+        [TestMethod]
+        public void TestIsLeapYear_Leap400()
+        {
+            var date = new Date();
+            Assert.AreEqual(true, date.isLeapYear(2000), "Pogreska u izracunu prijestupne godine");
+        }
+
+        [TestMethod]
+        public void TestIsLeapYear_NotLeapYear1()
+        {
+            var date = new Date(2, 2, 2014);
+            Assert.AreEqual(false, date.isLeapYear(), "Pogreska u izracunu prijestupne godine");
+        }
+
+        [TestMethod]
+        public void TestIsLeapYear_LeapYear1()
+        {
+            var date = new Date(2, 2, 2012);
+            Assert.AreEqual(true, date.isLeapYear(), "Pogreska u izracunu prijestupne godine");
+        }
+
+        [TestMethod]
+        public void TestIsLeapYear_Leap400_1()
+        {
+            var date = new Date(2, 2, 2000);
+            Assert.AreEqual(true, date.isLeapYear(), "Pogreska u izracunu prijestupne godine");
+        }
+
+        [TestMethod]
+        public void TestGetNumberOfRemainingDays_WithYear()
+        {
+            var date = new Date();
+            Assert.AreEqual(26, date.getNumberOfRemaingDaysInMonth(2, 2, 2014), "Pogreska u izracunu preostalih dana!");
+        }
+
+        [TestMethod]
+        public void TestGetNumberOfRemainingDays_WithLeapYear()
+        {
+            var date = new Date();
+            Assert.AreEqual(27, date.getNumberOfRemaingDaysInMonth(2, 2, 2012), "Pogreska u izracunu preostalih dana!");
+        }
+    }
+}
